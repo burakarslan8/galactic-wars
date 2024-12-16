db38eee323e0439ffec548d824710b8958032e25
burakarslan8 <burakarslan271@gmail.com>
2024-12-17 00:13:21 +0300

    Add weapon and bullet classes & shoot mechanic

---
+++ Scripts/bullet.gd
@@ -0,0 +1,45 @@
+class_name Bullet
+extends Area2D
+
+var _speed: float = 1000.0
+var _direction: Vector2 = Vector2.ZERO
+var _distance_traveled: float = 0.0
+
+var weapon: Weapon = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	set_as_top_level(true)
+
+func get_speed() -> float:
+	return _speed
+
+func set_speed(speed_value: float) -> void:
+	_speed = speed_value
+
+func get_direction() -> Vector2:
+	return _direction
+
+func set_direction(direction_value: Vector2) -> void:
+	_direction = direction_value
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var movement = _direction * get_speed() * delta
+	position += movement
+
+	_distance_traveled += movement.length()
+
+	if weapon and _distance_traveled >= weapon.get_range():
+		queue_free()
+
+func initialize_bullet(direction: Vector2, weapon: Weapon) -> void:
+	set_direction(direction.normalized())
+	self.weapon = weapon
+
+func get_damage() -> float:
+	var weapon = get_parent()
+	if weapon:
+		return weapon.get_damage()
+	return 0
--- Scripts/character.gd
+++ Scripts/character.gd
@@ -4,13 +4,17 @@ extends Node2D
 var _health: float = 100.0
 var _speed: float = 500.0
 var _velocity: Vector2 = Vector2.ZERO
+
 var camera: Camera2D = null
+var weapon: Weapon = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	camera = $Camera2D
 	if camera:
 		print("Camera attached to character")
+	
+	equip_weapon()
 
 func get_health() -> float:
 	return _health
@@ -37,9 +41,9 @@ func move(direction: Vector2, delta: float) -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	rotate_towards_mouse()
+	pass
 
-func rotate_towards_mouse() -> void:
-	var mouse_position = get_viewport().get_mouse_position()
-	var direction_to_mouse = (get_global_mouse_position() - global_position).normalized()
-	rotation = direction_to_mouse.angle() + PI / 2
+func equip_weapon() -> void:
+	weapon = preload("res://Scenes/Weapon.tscn").instantiate()
+	add_child(weapon)
+	weapon.position = Vector2(0, -40)
